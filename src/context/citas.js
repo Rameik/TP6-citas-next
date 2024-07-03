0ec692a2953b509bca224a8d355039f910d0c5ed
Rameik <ramakwon07@gmail.com>
2024-07-03 09:59:02 -0300

    tp7

--- src/context/citas.js
+++ src/context/citas.js
@@ -1,15 +1,17 @@
 'use client'
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import Cita from "../models/Cita";
 
 export const CitasContext = createContext()
 
 export function CitasProvider ({children}) {
-    const [citas, setCitas] = useState([
-        new Cita("Nina", "Martin", "2021-08-05", "08:20", "Le duele la pierna"),
+    const [citas, setCitas] = useState(JSON.parse(localStorage.getItem('citas')) ? JSON.parse(localStorage.getItem('citas')) : [new Cita("Nina", "Martin", "2021-08-05", "08:20", "Le duele la pierna"),
         new Cita("Sifon", "Flecha", "2023-08-05", "09:24", "Duerme mucho"),
-        new Cita("Floki", "Ari", "2023-08-05", "16:15", "No está comiendo")
-    ])
+        new Cita("Floki", "Ari", "2023-08-05", "16:15", "No está comiendo")])
+
+    useEffect(()=> {
+        localStorage.setItem('citas', JSON.stringify(citas))
+    }, [citas])
 
     return (
         <CitasContext.Provider value={{
