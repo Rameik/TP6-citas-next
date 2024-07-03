4a59394dbaf1c2273500ff025e34b6c9d4a6e667
Rameik <ramakwon07@gmail.com>
2024-07-03 10:12:43 -0300

    tp7

--- src/context/citas.js
+++ src/context/citas.js
@@ -5,12 +5,16 @@ import Cita from "../models/Cita";
 export const CitasContext = createContext()
 
 export function CitasProvider ({children}) {
-    const [citas, setCitas] = useState(JSON.parse(global?.window.localStorage.getItem('citas')) ? JSON.parse(global?.window.localStorage.getItem('citas')) : [new Cita("Nina", "Martin", "2021-08-05", "08:20", "Le duele la pierna"),
-        new Cita("Sifon", "Flecha", "2023-08-05", "09:24", "Duerme mucho"),
-        new Cita("Floki", "Ari", "2023-08-05", "16:15", "No está comiendo")])
+    const [citas, setCitas] = useState(null)
 
+    useEffect(() => {
+        setCitas(JSON.parse(window.localStorage.getItem('citas')) ? JSON.parse(window.localStorage.getItem('citas')) : [new Cita("Nina", "Martin", "2021-08-05", "08:20", "Le duele la pierna"),
+            new Cita("Sifon", "Flecha", "2023-08-05", "09:24", "Duerme mucho"),
+            new Cita("Floki", "Ari", "2023-08-05", "16:15", "No está comiendo")])
+    }, [])
+    
     useEffect(()=> {
-        global?.window.localStorage.setItem('citas', JSON.stringify(citas))
+        window.localStorage.setItem('citas', JSON.stringify(citas))
     }, [citas])
 
     return (
